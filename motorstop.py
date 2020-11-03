090d6bebc11117ac2423bcf4411e213e577a9823
Chris Evans <junasailor@gmail.com>
2020-11-02 21:23:27 -0600

    New Branch for larger testing

--- motorstop.py
+++ motorstop.py
@@ -15,10 +15,7 @@ from adafruit_motorkit import MotorKit
 
 kit = MotorKit(i2c=board.I2C())
 
-kit.motor1.throttle = 1.0
-time.sleep(1)
-kit.motor1.throttle = -1.0
-time.sleep(1)
-kit.motor1.throttle = 1.0
-time.sleep(1)
 kit.motor1.throttle = 0
+kit.motor2.throttle = 0
+kit.motor3.throttle = 0
+kit.motor4.throttle = 0
