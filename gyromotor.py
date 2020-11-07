3024d1bda21e393ca96119b70280e803ac1c4ec4
Chris Evans <junasailor@gmail.com>
2020-11-06 20:50:52 -0600

    Changed motors to Steppers and configured multiprocessing

--- gyromotor.py
+++ gyromotor.py
@@ -1,35 +1,83 @@
-#Basic config to control motor speed and left/right based on gyro position in space
-#using simple-pid for PID loop. https://pypi.org/project/simple-pid/
-#DC Motor at only 5 volts seems to be unable to turn freely if its given a signal less than 0.4, when it's at a full stop already.
-
+#!/usr/bin/env python3
 import time
 import board
 import busio
-from simple_pid import PID #get PID functions.
-from numpy import interp #interpolation for number scale mapping
-import adafruit_mpu6050 #for gyro
-from adafruit_motorkit import MotorKit #for motors
+import multiprocessing
+import queue
+import adafruit_mpu6050
+from adafruit_motorkit import MotorKit
+from adafruit_motor import stepper #stepper functionality, for doing stepper motors. I think we have to include this?
+from simple_pid import PID
 
-kit = MotorKit(i2c=board.I2C())
+
 i2c = busio.I2C(board.SCL, board.SDA)
+kit = MotorKit(i2c=board.I2C())
 mpu = adafruit_mpu6050.MPU6050(i2c)
-pid=PID(0.2,0.0,0.0, setpoint=0) #P,I,D gains and setpoint.
-#Kp affects how fast things respond. BAsed on where PV IS, and where SP needs it to be, and how far that is.
-#Ki Sums error difference, integral increases over time, until steady-state. even small error will make this term rise slowly over time.
-#Kd Makes the output decrease if PV is increasing FAST. Increading this paramter will make it react stronger to changes in Error. SHould be small.
+pid = PID(8,0,0, setpoint=0)   # PID loop to balance to zero
 
-pid.output_limits = (-1,1) #set limits, since output to motor speed only go 0 to 1 anyway, in either direction
-#mpu.acceleration is an array of x, y, z accleration values. Zero-indexed one dimensional array(?)
-#reference elements with a square bracket.
+pid.output_limits = (-100, 100) #range is limited up to "100%"
 
-#set motor to 0 throttle.
-#kit.motor1.throttle = 0
-#kit.motor3.throttle = 0
+global motorpercent #initialize the global
+oneshot=0   #debugging variable.
+motorsleep = 1 #initialize sleeptime. High number means slow speed/no movement.
 
-while True:
-    print(mpu.acceleration[1])
-    motorturn=pid(mpu.acceleration[1]) #PID tries to get the X acceleration to 0, established above. The outputis the speed of the motor.
-    #print(motorturn)
-    kit.motor1.throttle = motorturn
-    kit.motor3.throttle = -motorturn #reversed direction, because motor is moving "other" way
-    #time.sleep(0.1)
+#this is code for the child process.
+def wheelbalance(conn):
+    while True: #This may not be the best way to loop, but... best I have for now.
+#        try:
+#            pulse=conn.get() #this will receive the queue data into Pulse variable for this function
+#            print("queue Has Data")
+#        except:
+#            print("queue is empty")
+#            pass
+
+        pulse=conn.get()
+
+        #pulse=0 #setting this static to test the motors.
+
+        print("pulse is", pulse) #debugging.
+        steps=10 #do more than one step for smoothness.
+        #for i in (1000): #I dont want to make while TRUe, because runaway process? will find a way for this later.
+        #Dont turn motors if it's fallen over.
+        if abs(mpu.acceleration[1]) > 8:
+            kit.stepper1.release()
+            kit.stepper2.release()
+        elif mpu.acceleration[1] > 0: #direction setting here. adjust based on wiring.
+            for i in range(steps): #do a few steps
+                kit.stepper1.onestep(direction=stepper.FORWARD, style=stepper.SINGLE)
+                kit.stepper2.onestep(direction=stepper.FORWARD, style=stepper.SINGLE)
+                time.sleep(pulse)
+        else:
+            for i in range(steps): #do a few steps
+                kit.stepper1.onestep(direction=stepper.BACKWARD, style=stepper.SINGLE)
+                kit.stepper2.onestep(direction=stepper.BACKWARD, style=stepper.SINGLE)
+                time.sleep(pulse)
+
+
+#start the second process, to run the stepper motors:
+queue = multiprocessing.Queue() #setup queue to send info.
+wheelcontrol = multiprocessing.Process(target=wheelbalance, args=(queue,))
+wheelcontrol.start() #starts this child process. How does it stop?
+
+
+###########
+#The main code here:
+###########
+
+while oneshot<100000:
+    #first check PID and get values
+    motorpercent = pid(mpu.acceleration[1]) #global so that the wheel process can see this
+    motorsleep = 0.01-(abs(motorpercent)/10000)
+
+    #send new timing value to the child process, for motor control.
+    if oneshot < 100000:
+        queue.put(motorsleep)
+        oneshot = oneshot+1 #so this if won't run again.
+        print("sending...")
+
+    #print for debugging:
+    #print(mpu.acceleration[1])
+    #print(motorpercent)
+    #print(motorsleep)
+
+    #time.sleep(1) #general sleep for the main process to slow down, and print etc.
